a8e76934c2f0dd1bb423cd7083ae800eea83a60a
Extomvi <exceltadedokun@gmail.com>
2022-08-03 08:51:46 +0000

    Update to the recursive function

--- reverse_string.py
+++ reverse_string.py
@@ -12,6 +12,7 @@ def reverseString(s):
         if left < right:
             s[left], s[right] = s[right], s[left]
             left, right = left + 1, right - 1
+            helper(left, right)
 
     helper(0, len(s) - 1)
     return s
