650a377701422d0d653287dea04c9a184f6b5827
Extomvi <exceltadedokun@gmail.com>
2022-08-04 14:02:06 +0000

    Update valid_palindrome.py

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,18 +1,18 @@
 """Check if a sentence is a valid palindrome"""
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        word = []
-        for i in s:
-            if (
-                ord('A')<=ord(i)<=ord('Z') 
-                or ord('a')<=ord(i)<=ord('z') 
-                or ord('0')<=ord(i)<=ord('9')
-            ):
-                word.append(i.lower())
-                
-        left, right = 0, len(word) - 1
-        while left < right:
-            if word[left] != word[right]:
-                return False
-            left, right = left + 1, right - 1
-        return True 
+def isPalindrome(s):
+    word = []
+    for i in s:
+        if (
+            ord('A')<=ord(i)<=ord('Z') 
+            or ord('a')<=ord(i)<=ord('z') 
+            or ord('0')<=ord(i)<=ord('9')
+        ):
+            word.append(i.lower())
+            
+    left, right = 0, len(word) - 1
+    while left < right:
+        if word[left] != word[right]:
+            return False
+        left, right = left + 1, right - 1
+    return True 
+print(isPalindrome("A man, a plan, a canal: Panama"))
