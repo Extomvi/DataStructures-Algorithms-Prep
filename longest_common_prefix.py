ddad139f731101a98209175b52cd0cdf046f7406
Extomvi <exceltadedokun@gmail.com>
2022-08-04 11:09:26 +0000

    Created longestCommonPrefix

---
+++ longest_common_prefix.py
@@ -0,0 +1,19 @@
+"""
+Longest common prefix in a list
+"""
+def longestCommonPrefix(strs):
+    if len(strs) == 1:
+        return strs[0]
+    strs.sort()
+    result = ""
+    first = strs[0]
+    last = strs[len(strs)-1]
+    print(first, last)
+    for i in range(len(first)):
+        if first[i] ==  last[i]:
+            result = result + first[i]
+        else:
+            break
+    return result
+
+longestCommonPrefix(["leetcode", "leets", "leetc", "lettuce"])
