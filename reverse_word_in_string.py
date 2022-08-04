9834cde9e14a782f35e5c355fd4b6710379d042d
Extomvi <exceltadedokun@gmail.com>
2022-08-04 03:47:28 +0100

    Update reverse_word

--- reverse_word_in_string.py
+++ reverse_word_in_string.py
@@ -1,10 +1,9 @@
 """Reverse word in a string III"""
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        rev_string = s[::-1]
-        revString = rev_string.split(" ")
-        revString = revString[::-1]
-        
-        return " ".join(revString)
+def reverseWords(s):
+    rev_string = s[::-1]
+    revString = rev_string.split(" ")
+    revString = revString[::-1]
+
+    return " ".join(revString)
  
-        
+print(reverseWords("running around"))
