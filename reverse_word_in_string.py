d4c135b1bdb6eda79496ba60efe4cf47acb03dce
Extomvi <exceltadedokun@gmail.com>
2022-08-04 03:51:32 +0100

    Modified reverse_word code

--- reverse_word_in_string.py
+++ reverse_word_in_string.py
@@ -1,4 +1,12 @@
 """Reverse word in a string III"""
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        rev_string = (
+             s[::-1]               # Character reverse
+             .split(' ')[::-1]     # Array reverse
+        )
+        return " ".join(rev_string)
+        
 def reverseWords(s):
     rev_string = s[::-1]
     revString = rev_string.split(" ")
