5083451abf63b0e5e92a86c4bfca3e124252391b
Extomvi <exceltadedokun@gmail.com>
2022-08-06 20:16:08 +0100

    Created two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,22 @@
+"""
+Two sum
+"""
+#Brute force O(n2)
+def twoSum(nums, target):
+    #bruteforce solution
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            val = target - nums[i]
+            if nums[j] == val:
+                return [i,j]
+
+def twoSum(nums, target):
+    #Using a dictionary O(N)
+    seen = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in seen:
+            return [seen[complement], i]
+        seen[nums[i]] = i
+
+print(twoSum([2,7,11,15],9))
