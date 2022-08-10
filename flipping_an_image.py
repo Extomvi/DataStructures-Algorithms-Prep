f2f0ce65e79a81f86324ffb3745110753e52ccf7
Extomvi <exceltadedokun@gmail.com>
2022-08-10 16:51:35 +0000

    Updated flipping_image.py fille

--- flipping_an_image.py
+++ flipping_an_image.py
@@ -4,16 +4,39 @@ Given an n x n binary matrix image, flip the image horizontally, then invert it,
 """
 
 # image = [[1,1,0],[1,0,1],[0,0,0]]
+"""
+image = 
+[
+    [1,1,0],
+    [1,0,1],
+    [0,0,0]
+]
+After inverting each item in the list using [::-1]
+    |
+    V
+[
+    [0,1,1],
+    [1,0,1],
+    [0,0,0]
+]
+Using lambda function to change each item in the row to the opposit(1 to 0); Becomes -> 
+[
+    [1,0,0],
+    [0,1,0],
+    [1,1,1]
+]
+"""
 #Becomes -> [[1,0,0],[0,1,0],[1,1,1]]
 
-from operator import inv
 
 
 def flippingImage(image):
+    result = []
     for row in image:
         #print(row)
+        #print(row[::-1])
         inverted = lambda x: 0 if x == 1 else 1
-        result = (list(map(inverted, item)) for item in row[::-1])
-        return result
+        result.append(list(map(inverted, row[::-1])))
+    return result
 
 print(flippingImage([[1,1,0],[1,0,1],[0,0,0]]))
