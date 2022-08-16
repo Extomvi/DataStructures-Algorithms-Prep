c8531cda3b0a33abf3f2c5dfd75efabd78bfc52d
Extomvi <exceltadedokun@gmail.com>
2022-08-16 16:02:51 +0000

    Hashmap implementation

--- hashmap.py
+++ hashmap.py
@@ -2,6 +2,9 @@
 Implementing an hash map/ hash table
 """
 
+from multiprocessing.pool import RUN
+
+
 class HashMap:
 
     def __init__(self,size):
@@ -12,4 +15,69 @@ class HashMap:
         return [[] for i in range(self.size)]
 
     def set_val(self, key, val):
-        
+        hash_key = hash(key) % self.size
+
+        bucket = self.hash_map[hash_key]
+
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+
+            if record_key == key:
+                found_key = True
+                break
+
+        if found_key:
+            bucket[index] = (key, val)
+        else:
+            bucket.append((key,val))
+
+    def get_val(self, key):
+        hash_key = hash(key) % self.size
+
+        bucket = self.hash_map[hash_key]
+
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+
+            if record_key == key:
+                found_key = True
+                break
+
+        if found_key:
+            return record_val
+        else:
+            return "No record found"
+
+
+    def delete_val(self, key):
+        hash_key = hash(key) % self.size
+
+        bucket = self.hash_map[hash_key]
+
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+
+            if record_key == key:
+                found_key = True
+                break
+
+        if found_key:
+            bucket.pop(index)
+        return
+
+    def __str__(self):
+        return "".join(str(item) for item in self.hash_map)
+
+hash_map = HashMap(5)
+
+hash_map.set_val('run@gmail.com', 'RUN')
+print(hash_map)
+hash_map.set_val('wing@gmail.com', 'WING')
+print(hash_map)
+print(hash_map.get_val('wing@gmail.com'))
+hash_map.delete_val('run@gmail.com')
+print(hash_map)
+
