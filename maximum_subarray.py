698abec91b0e4408a205e5bf8e392196e3d00cd4
Extomvi <exceltadedokun@gmail.com>
2022-08-18 15:49:54 +0000

    Created maximum_subarray.py

---
+++ maximum_subarray.py
@@ -0,0 +1,18 @@
+"""
+Maximum subarray
+"""
+#Using a sliding window to get a O(N) algorithm
+def maximum_subarray(nums):
+    maxSum = nums[0]
+    currentSum = 0
+
+    for n in nums:
+        if currentSum < 0:
+            currentSum = 0
+        currentSum += n
+        maxSum = max(maxSum, currentSum)
+    return maxSum
+        
+
+
+print(maximum_subarray([5,4,-1,7,8]))
