fcc2671df16cd03b0519ed6329b5a235231d0f6c
Extomvi <exceltadedokun@gmail.com>
2022-08-24 15:13:56 +0000

    Created queue.py file

---
+++ queue.py
@@ -0,0 +1,50 @@
+"""
+Implementing a queue (FIFO)
+"""
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+class Queue:
+    def __init__(self):
+        self.first = Node("first")
+        self.last = Node("last")
+        self.size = 0
+
+    def __str__(self):
+        cur = self.first.next
+        out = ""
+        while cur:
+            out += str(cur.value) + " -> "
+            cur = cur.next
+        return out[:-3]
+
+    def add(self, value):
+        node = Node(value)
+        if self.last.next != None:
+            self.last.next.next = node
+        self.last.next = node
+        if self.first.next == None:
+            self.first.next = self.last.next
+        self.size += 1
+
+    def remove(self):
+        if self.first == None:
+            raise Exception("No such element")
+        remove = self.first.next.value
+        self.first.next = self.first.next.next
+        if self.first.next == None:
+            self.last.next == None
+        self.size -= 1
+        return remove
+
+if __name__ == "__main__":
+    stack = Queue()
+    for i in range(1,11):
+        print(i)
+        stack.add(i)
+    print(f"After adding to the queue: {stack}")
+    if stack.size != 0:
+        stack.remove()
+    print(f"After popping: {stack}") 
+        
