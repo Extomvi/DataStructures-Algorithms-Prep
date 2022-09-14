8c17c69a4974aac9cfdeae42ccc9b94322a9b269
Extomvi <exceltadedokun@gmail.com>
2022-09-14 16:12:05 +0000

    AddedclimbingStairs.py file

---
+++ climbing_stairs.py
@@ -0,0 +1,24 @@
+"""
+Climbing stairs problem
+"""
+
+# Leetcode: https://leetcode.com/problems/climbing-stairs/
+
+def climbingStairs(n):
+    """
+                n
+                /\
+               1  2
+              /\  /\
+             1  2 1 2
+    """
+    one, two = 1, 1
+
+    for i in range(n - 1):
+        temporary = one
+        one = one + two
+        one = temporary
+
+    return one
+
+print(climbingStairs(2))
