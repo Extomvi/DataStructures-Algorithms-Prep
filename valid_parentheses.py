de283e6b09318fe75bddbce62f33dfd160880c57
Tomiwa <exceltadedokun@gmail.com>
2022-09-28 11:30:37 +0000

    Updated to include mapping implementation

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -33,10 +33,28 @@ def validParentheses(symbolString):
     
     else:
         return False
+
+def isValid(s):
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+        
+    for char in s:
+        
+        if char in mapping:
+            
+            top = stack.pop() if stack else "*"
+            
+            if mapping[char] != top:
+                return False
+            
+        else:
+            stack.append(char)
+            
+    return not stack
     
 
 print(validParentheses('{({([][])}())}'))
-print(validParentheses('[{()]'))
+print(isValid('[{()]'))
 
 #For same type of braces
 def valid_parenthesis(symbolString):
