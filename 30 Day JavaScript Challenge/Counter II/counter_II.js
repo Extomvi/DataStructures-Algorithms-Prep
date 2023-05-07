314db085c95127c67dd62c036cee0fe16b2c676c
Tomiwa <exceltadedokun@gmail.com>
2023-05-07 21:56:57 +0000

    task: added solution in js

---
+++ 30 Day JavaScript Challenge/Counter II/counter_II.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init;
+
+    function increment(){
+        return ++count;
+    }
+    function decrement(){
+        return --count;
+    }
+    function reset(){
+        return count = init;
+    }
+    return {increment, decrement, reset}
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
