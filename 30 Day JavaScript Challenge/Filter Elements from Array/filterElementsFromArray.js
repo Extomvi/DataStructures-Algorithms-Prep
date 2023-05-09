2583e3580b1520f0582d4cb18c8c25a0b3f1530e
Tomiwa <exceltadedokun@gmail.com>
2023-05-09 21:10:11 +0000

    task: added solution to problem

--- 30 Day JavaScript Challenge/Filter Elements from Array/filterElementsFromArray.js
+++ 30 Day JavaScript Challenge/Filter Elements from Array/filterElementsFromArray.js
@@ -3,6 +3,19 @@
  * @param {Function} fn
  * @return {number[]}
  */
+// Using Imperative programming
 var filter = function(arr, fn) {
-    
+    const result = [];
+
+    for(let i = 0; i < arr.length; i++){
+        if(fn(arr[i], i)){
+            result.push(arr[i]);
+        }
+    }
+    return result;
+};
+
+// Using the filter function (declarative programming)
+var filter = function(arr, fn) {
+    return arr.filter(fn);  
 };
