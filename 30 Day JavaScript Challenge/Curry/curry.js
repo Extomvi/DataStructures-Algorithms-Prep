b04e1096e9603cf551d77d66e87a377183622fe2
Tomiwa <exceltadedokun@gmail.com>
2023-05-14 18:46:22 +0000

    task: added solution template

---
+++ 30 Day JavaScript Challenge/Curry/curry.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var curry = function(fn) {
+    return function curried() {
+
+    };
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
