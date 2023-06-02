1b9c495d73c12dda05e46889c80929cee2960611
Tomiwa <exceltadedokun@gmail.com>
2023-06-02 21:51:43 +0000

    task: updated comments on code

--- invalid_transactions.py
+++ invalid_transactions.py
@@ -1,16 +1,9 @@
 """
 Invalid Transaction:https://leetcode.com/problems/invalid-transactions/
-
-A transaction is possibly invalid if:
-
-the amount exceeds $1000, or;
-if it occurs within (and including) 60 minutes of another transaction with the same name in a different city.
-You are given an array of strings transaction where transactions[i] consists of comma-separated values representing the name, time (in minutes), amount, and city of the transaction.
-
-Return a list of transactions that are possibly invalid. You may return the answer in any order.
 """
 
 from collections import defaultdict
+
 #First way
 def invalidTrans(transactions):
     invalid = {}
@@ -46,6 +39,7 @@ def invalidTrans(transactions):
 
     return result
 
+# Method with sorting
 def invalidTransaction(transactions):
     invalid = defaultdict(list)
     result = set()
